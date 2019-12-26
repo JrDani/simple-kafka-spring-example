5f91a405a911f235f8fc8cd73d01fbf5db88dcc6
Daniel Silva <daniel_silva@trimble.com>
2019-12-26 17:25:43 -0300

    First commit

---
+++ src/main/java/app/KotlinProducer.kt
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class KotlinProducer(private val kafkaTemplate: KafkaTemplate<String, String>) {
+
+    fun send(message: String) {
+        kafkaTemplate.send("simple-message-topic", message)
+    }
+}
